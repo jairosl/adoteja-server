4d4df23bd701128ed93ce135d19f0d76966fa744
jairosl <jairo.lima@aluno.uepb.edu.br>
2020-08-15 13:42:42 -0300

    Check husky lint-staged

--- src/server.js
+++ src/server.js
@@ -2,10 +2,8 @@ import express from 'express';
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ ok: true  });
-})
+app.get('/', (request, response) => response.json({ ok: true }));
 
 app.listen(3333, () => {
-  console.log("Server running on port 3333");
-})
+  console.log('Server running on port 3333');
+});
