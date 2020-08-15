fa81f90836f36df23cbab1c727cbc92879e0dfd2
jairosl <jairo.lima@aluno.uepb.edu.br>
2020-08-15 13:45:59 -0300

    Check lint-staged

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => response.json({ ok: true }));
+app.get('/', (request, response) => response.json({ ok: false }));
 
 app.listen(3333, () => {
   console.log('Server running on port 3333');
