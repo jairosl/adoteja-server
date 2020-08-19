7329c254157887166a00be51ce4618a47185e96f
jairosl <jairo.lima@aluno.uepb.edu.br>
2020-08-19 13:33:47 -0300

    feat: add cors in the aplication

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 
 require('dotenv').config();
@@ -6,6 +7,7 @@ require('dotenv').config();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use(routes);
 
